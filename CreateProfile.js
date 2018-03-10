05f08a360218f4f10933a092779fed9507f3e25b
sgalutira <sheena.galutira@gmail.com>
2018-03-09 22:13:08 -1000

    separated components to different files

---
+++ CreateProfile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StackNavigator,
+} from 'react-navigation';
+
+class CreateProfileScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Profile Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default CreateProfileScreen;
